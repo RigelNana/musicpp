3e047fb013070b2328fd2336cf16a2508d960976
RigelShrimp <809239189@qq.com>
2026-02-25 20:00:26 +0800

    feat: add unified header file

---
+++ include/musicpp/musicpp.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "chord_sequence.hpp"
+#include "chords.hpp"
+#include "degree.hpp"
+#include "duration.hpp"
+#include "intervals.hpp"
+#include "notes.hpp"
+#include "progressions.hpp"
+#include "scales.hpp"
+#include "timing.hpp"
+#include "melody.hpp"
